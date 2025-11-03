e079e618a8104ab4632e76c0daa201af37e9cd65
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-03 13:12:00 -0300

    feat: adicionado componente SelectedFlight para exibir detalhes do voo e renderizar RewardsCard

---
+++ frontend/src/pages/SelectedFlight.tsx
@@ -0,0 +1,59 @@
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { api } from "../services/api";
+import type { Flight } from "../types/flight";
+
+import { PilopsLogo } from "../components/PilopsLogo";
+import { RewardsCard } from "../components/RewardsCard";
+
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { Box, Typography } from "@mui/material";
+
+export const SelectedFlight = () => {
+    const { id } = useParams();
+    const location = useLocation();
+    const [flight, setFlight] = useState<Flight | null>(location.state || null);
+
+    useEffect(() => {
+        if (!flight) {
+            api.get(`/flights/${id}`).then((response) => {
+                setFlight(response.data.flight);
+            });
+        }
+    }, [id, flight]);
+
+    if (!flight) return <div>Carregando...</div>;
+
+    return (
+        <Box
+            sx={{
+                maxWidth: "1200px",
+                mx: "auto",
+                px: 2,
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+            }}
+        >
+            <PilopsLogo />
+            <Box
+                sx={{ maxWidth: "1200px", display: "flex"}}
+            >
+                <ArrowBackIosIcon />
+                <Typography
+                    sx={{
+                        fontFamily: "Sora, sans-serif",
+                        fontWeight: 700,
+                        fontSize: "24px",
+                        lineHeight: "110%",
+                        letterSpacing: "0",
+                    }}
+                >
+                    Detalhes do Voo
+                </Typography>
+            </Box>
+            <RewardsCard flight={flight} />
+        </Box>
+    );
+};
