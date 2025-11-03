618e45574bf2b6e36cb1429cf48c1953097ee8d0
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-03 15:20:56 -0300

    feat: implementada paginação e estrutura de resposta no endpoint getAllFlights, alterado o nome da importação fligths para flightsData para melhor entendimento

--- backend/src/controllers/flightController.ts
+++ backend/src/controllers/flightController.ts
@@ -1,24 +1,39 @@
 import type { Request, Response } from 'express';
-import flights from '../data/flightHistory.json' with { type: 'json' };
+import flightsData from '../data/flightHistory.json' with { type: 'json' };
 
 export const getAllFlights  = (req: Request, res: Response): Response => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const flightsPage = flightsData.flights.slice(startIndex, endIndex);
+
+    const totalFlights = flightsData.flights.length;
+    const totalPages = Math.ceil(totalFlights/limit);
+
     try {
-        return res.status(200).json(flights);
+        return res.status(200).json({
+            page,
+            limit,
+            totalFlights,
+            totalPages,
+            flights: flightsPage});
     } catch (error) {
         return res.status(500).json({ message: 'Erro ao carregar voos.' });
     }
 };
 
 export const getFlightById = (req: Request, res: Response) => {
-    const { flights: flightList } = flights;
+    const { flights: flightList } = flightsData;
     
     try {
         const { id } = req.params;
-        const flight = flightList.find(f => f.id === id);
-        if (!flight) {
+        const flightById = flightList.find(f => f.id === id);
+        if (!flightById) {
             return res.status(404).json({ message: 'Voo não encontrado.' });
         } 
-        return res.status(200).json(flight);
+        return res.status(200).json(flightById);
     } catch (error) {
         return res.status(500).json({ message: 'Erro ao carregar voo.' });
     }
