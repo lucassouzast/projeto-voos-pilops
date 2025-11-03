53de07502e19946d74847b20c6a641ad41f08a3e
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-02 21:46:34 -0300

    feat: adicionado ThemeProvider e CssBaseline para integração do tema no App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import '@fontsource/manrope/400.css'
+import '@fontsource/manrope/500.css'
+import '@fontsource/manrope/700.css'
+import '@fontsource/sora/400.css'
+import '@fontsource/sora/500.css'
+import '@fontsource/sora/700.css'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
