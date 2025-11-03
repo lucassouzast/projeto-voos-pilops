51a0b9a6f46e28b2fe8ad4982ae01fc390e44a16
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-02 23:14:53 -0300

    feat: adicionado suporte ao CORS e atualizado @types/cors nas dependências

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import helmet from 'helmet';
 import flightsRoutes from './routes/flightsRoutes.js';
-
+import cors from 'cors';
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 server.use('/', flightsRoutes);
 
 server.listen(3000, () => {
