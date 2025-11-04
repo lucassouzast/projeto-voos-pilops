c972a440a6cd3ef7b12553cf3901251238a44119
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-04 13:37:33 -0300

    feat: criação do model Flight para typagem e organização do controller de voos e melhorar legibilidade.

--- backend/src/controllers/flightController.ts
+++ backend/src/controllers/flightController.ts
@@ -1,15 +1,19 @@
 import type { Request, Response } from 'express';
 import flightsData from '../data/flightHistory.json' with { type: 'json' };
+import { Flight } from '../model/flightModel';
 
-export const getAllFlights  = (req: Request, res: Response): Response => {
+
+const flights: Flight[] = flightsData.flights;
+
+export const getAllFlights  = (req: Request, res: Response) => {
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
-    const flightsPage = flightsData.flights.slice(startIndex, endIndex);
+    const flightsPage = flights.slice(startIndex, endIndex);
 
-    const totalFlights = flightsData.flights.length;
+    const totalFlights = flights.length;
     const totalPages = Math.ceil(totalFlights/limit);
 
     try {
@@ -25,15 +29,14 @@ export const getAllFlights  = (req: Request, res: Response): Response => {
 };
 
 export const getFlightById = (req: Request, res: Response) => {
-    const { flights: flightList } = flightsData;
     
     try {
         const { id } = req.params;
-        const flightById = flightList.find(f => f.id === id);
+        const flightById = flights.find(f => f.id === id);
         if (!flightById) {
             return res.status(404).json({ message: 'Voo não encontrado.' });
         } 
-        return res.status(200).json({flight:flightById});
+        return res.status(200).json({flightById});
     } catch (error) {
         return res.status(500).json({ message: 'Erro ao carregar voo.' });
     }
@@ -41,8 +44,7 @@ export const getFlightById = (req: Request, res: Response) => {
 
 
 export const getAllFlightsBalance = (req: Request, res:Response) => {
-    const {flights: flightsBalance } = flightsData;
 
-    const totalBalance = flightsBalance.reduce((acc, f) => acc + f.flightData.balance, 0);
+    const totalBalance = flights.reduce((acc, f) => acc + f.flightData.balance, 0);
     return res.status(200).json({totalBalance});
 }
