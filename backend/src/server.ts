433fb3b5aa09ad56fae1d18373a0aeb8e20f9726
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-04 17:24:12 -0300

    feat: ajustado o servidor para usar variável para a porta

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,8 @@ server.use(express.json());
 server.use(cors());
 server.use('/', flightsRoutes);
 
-server.listen(3000, () => {
-    console.log('Servidor rodando: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando: http://localhost:${PORT}`);
 })
