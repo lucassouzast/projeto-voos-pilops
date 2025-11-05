f68a0fa4c081cfe2f180af3c24aae484ae568ce6
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-05 20:23:45 -0300

    fix: corrigido endpoint de saldo e adicionado tratamento de erro no serviço de voos incluido feat: chamada por Id para componentização

--- frontend/src/services/flightServices.ts
+++ frontend/src/services/flightServices.ts
@@ -2,10 +2,11 @@ import { api } from "./api";
 
 export const getTotalBalance = async () => {
     try {
-        const response = await api.get("/flights/balance");
+        const response = await api.get("/flights/b2alance");
         return response.data.totalBalance;
     } catch (error) {
         console.error("Erro ao buscar saldo:", error);
+        throw error;
     }
 };
 
@@ -18,3 +19,12 @@ export const getFlights = async (page: number) => {
         throw error;
     }
 };
+
+export const getFlightById = async (id: string) => {
+    try {
+        const response = await api.get(`/flights/${id}`);
+        return response.data.flightById;
+    } catch (error) {
+        console.error("Erro ao buscar voo:", error);
+        throw error;
+    }};
