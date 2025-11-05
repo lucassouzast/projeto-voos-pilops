748b2c350ab95a4c116fc152c693992c92076156
Lucas Pereira de Souza <lucassouza6932@gmail.com>
2025-11-05 20:26:05 -0300

    feat: adicionar tratamento de erro e componente ErrorAlert ao FlightBalance

--- frontend/src/components/FlightBalance.tsx
+++ frontend/src/components/FlightBalance.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Box, Typography, Skeleton } from "@mui/material";
 import { getTotalBalance } from "../services/flightServices";
+import { ErrorAlert } from "./ErrorAlert";
 
 export const FlightBalance = () => {
     const [totalBalance, setTotalBalance] = useState(0);
     const [loading, setLoading] = useState(true);
+
+    const [error, setError] = useState(false);
 
     const fetchBalance = async () => {
         try {
@@ -13,6 +16,7 @@ export const FlightBalance = () => {
             setLoading(false);
         } catch (error) {
             console.error(error);
+            setError(true);
             setLoading(false);
         }
     };
@@ -21,43 +25,61 @@ export const FlightBalance = () => {
         fetchBalance();
     }, []);
 
+    const handleCloseError = () => {
+        setError(false);
+    };
     return (
-        loading ? (
-            <Skeleton
-                variant="rectangular"
-                width={250}
-                height={80}
-                sx={{ borderRadius: 2, ml: 0.5 }}
+        <>
+            <ErrorAlert
+                error={error}
+                onClose={handleCloseError}
+                message="Erro ao buscar o saldo total dos voos"
             />
-        ) : (
-            <Box
-                display={"flex"}
-                justifyContent={"center"}
-                sx={{
-                    bgcolor: "#1976d2",
-                    color: "#fff",
-                    borderRadius: 2,
-                    maxHeight: "80px",
-                    width: { sm: "250px" },
-                    px: { xs: 2.5, sm: 0 },
-                    ml: 0.5,
-                    py:1
-                }}
-            >
-                <div>
-                    <Typography variant="overline">Saldo Total</Typography>
-                    <Typography
-                        variant="h5"
-                        component="div"
-                        sx={{ fontWeight: 700, whiteSpace: "nowrap" }}
-                    >
-                        {`P$ ${totalBalance.toLocaleString("pt-BR", {
-                            minimumFractionDigits: 2,
-                            maximumFractionDigits: 2,
-                        })}`}
-                    </Typography>
-                </div>
-            </Box>
-        )
+            {loading ? (
+                <Skeleton
+                    variant="rectangular"
+                    width={250}
+                    height={80}
+                    sx={{ borderRadius: 2, ml: 0.5 }}
+                />
+            ) : (
+                <Box
+                    display={"flex"}
+                    justifyContent={"center"}
+                    sx={{
+                        bgcolor: "#1976d2",
+                        color: "#fff",
+                        borderRadius: 2,
+                        maxHeight: "80px",
+                        width: { sm: "250px" },
+                        px: { xs: 2.5, sm: 0 },
+                        ml: 0.5,
+                        py: 1,
+                    }}
+                >
+                    {error ? (
+                        <Typography variant="overline">
+                            Erro ao carregar saldo
+                        </Typography>
+                    ) : (
+                        <div>
+                            <Typography variant="overline">
+                                Saldo Total
+                            </Typography>
+                            <Typography
+                                variant="h5"
+                                component="div"
+                                sx={{ fontWeight: 700, whiteSpace: "nowrap" }}
+                            >
+                                {`P$ ${totalBalance.toLocaleString("pt-BR", {
+                                    minimumFractionDigits: 2,
+                                    maximumFractionDigits: 2,
+                                })}`}
+                            </Typography>
+                        </div>
+                    )}
+                </Box>
+            )}
+        </>
     );
 };
